be2c0fddff28f9e92088456b4c87f579b67fa054
Rémy Beumier <beumieremy@gmail.com>
2018-01-16 20:31:09 +0100

    memory removed jquery

--- memory/README.txt
+++ memory/README.txt
@@ -1,5 +1,5 @@
 A Pen created at CodePen.io. You can find this one at https://codepen.io/beumsk/pen/xdoKjX.
 
- A memory game created with html css js and jquery.
+ A memory game created with html, css and javascript.
 Chose between different themes.
 Be as fast as possible and spare your clicks !
--- memory/js/index.js
+++ memory/js/index.js
@@ -1,7 +1,7 @@
-
-// click twice on same image problem !
+ 
+// host images on cloudinary !
 // other themes to add (HP, )
-// bigger memory
+// bigger memory?
 
 var pokemon = [
   'http://cdn.bulbagarden.net/upload/thumb/2/21/001Bulbasaur.png/250px-001Bulbasaur.png',
@@ -25,7 +25,7 @@ var pokemon = [
   'http://cdn.bulbagarden.net/upload/thumb/1/1b/146Moltres.png/250px-146Moltres.png',
   'http://cdn.bulbagarden.net/upload/thumb/e/e2/133Eevee.png/250px-133Eevee.png'
 ];
-var starWars = [
+var starwars = [
   'http://www.starwars-universe.com/images/encyclopedie/personnages/avatars_v6/anakinskywalkerava.jpg',
   'http://www.starwars-universe.com/images/encyclopedie/personnages/avatars_v6/luke_avv6.jpg',
   'http://www.starwars-universe.com/images/encyclopedie/personnages/avatars_v6/obi_avv6.jpg',
@@ -71,25 +71,25 @@ var lotr = [
 ];
 var disney = [
   'http://www.popstickers.fr/4044-home_default/mickey.jpg', 
-	'http://rs112.pbsrc.com/albums/n185/modest_me9/mowgli.jpg~c200',
-	'https://qph.ec.quoracdn.net/main-thumb-t-29038-200-80YknGmQ2R9Hk9bEy43s7dHXNpvJtV8s.jpeg',
-	'http://img.kbhgames.com/2016/07/Tarzan-Return-to-the-Jungle.jpg',
-'http://rs951.pbsrc.com/albums/ad356/MetallicAerin2/TLKpart3001_0001.jpg~c200',
-	'https://s-media-cache-ak0.pinimg.com/736x/c4/df/97/c4df974ed6a9144b38e31360912898eb.jpg',
-	'http://ekladata.com/aNG7q920ZA59GLvdh0SjQhXrWMs@200x200.jpg',
-	'http://ekladata.com/-jQr-XuNktvvO6f4lo3CMMVK7ew@200x200.png',
-'http://neverpedia.com/wiki/images/thumb/2/21/DisneyPeterPan.jpg/200px-DisneyPeterPan.jpg',
-'https://www.ecranlarge.com/media/cache/200x200/uploads/image/000/938/pinocchio-photo-pinocchio-938230.jpg',
-	'http://www.popstickers.fr/4044-home_default/mickey.jpg', 
-'http://rs112.pbsrc.com/albums/n185/modest_me9/mowgli.jpg~c200',
-	'https://qph.ec.quoracdn.net/main-thumb-t-29038-200-80YknGmQ2R9Hk9bEy43s7dHXNpvJtV8s.jpeg',
-	'http://img.kbhgames.com/2016/07/Tarzan-Return-to-the-Jungle.jpg',
-'http://rs951.pbsrc.com/albums/ad356/MetallicAerin2/TLKpart3001_0001.jpg~c200',
-	'https://s-media-cache-ak0.pinimg.com/736x/c4/df/97/c4df974ed6a9144b38e31360912898eb.jpg',
-'http://ekladata.com/aNG7q920ZA59GLvdh0SjQhXrWMs@200x200.jpg',
-	'http://ekladata.com/-jQr-XuNktvvO6f4lo3CMMVK7ew@200x200.png',
-'http://neverpedia.com/wiki/images/thumb/2/21/DisneyPeterPan.jpg/200px-DisneyPeterPan.jpg',
-'https://www.ecranlarge.com/media/cache/200x200/uploads/image/000/938/pinocchio-photo-pinocchio-938230.jpg'
+  'http://rs112.pbsrc.com/albums/n185/modest_me9/mowgli.jpg~c200',
+  'https://qph.ec.quoracdn.net/main-thumb-t-29038-200-80YknGmQ2R9Hk9bEy43s7dHXNpvJtV8s.jpeg',
+  'http://img.kbhgames.com/2016/07/Tarzan-Return-to-the-Jungle.jpg',
+  'http://rs951.pbsrc.com/albums/ad356/MetallicAerin2/TLKpart3001_0001.jpg~c200',
+  'https://s-media-cache-ak0.pinimg.com/736x/c4/df/97/c4df974ed6a9144b38e31360912898eb.jpg',
+  'http://ekladata.com/aNG7q920ZA59GLvdh0SjQhXrWMs@200x200.jpg',
+  'http://ekladata.com/-jQr-XuNktvvO6f4lo3CMMVK7ew@200x200.png',
+  'http://neverpedia.com/wiki/images/thumb/2/21/DisneyPeterPan.jpg/200px-DisneyPeterPan.jpg',
+  'https://www.ecranlarge.com/media/cache/200x200/uploads/image/000/938/pinocchio-photo-pinocchio-938230.jpg',
+  'http://www.popstickers.fr/4044-home_default/mickey.jpg', 
+  'http://rs112.pbsrc.com/albums/n185/modest_me9/mowgli.jpg~c200',
+  'https://qph.ec.quoracdn.net/main-thumb-t-29038-200-80YknGmQ2R9Hk9bEy43s7dHXNpvJtV8s.jpeg',
+  'http://img.kbhgames.com/2016/07/Tarzan-Return-to-the-Jungle.jpg',
+  'http://rs951.pbsrc.com/albums/ad356/MetallicAerin2/TLKpart3001_0001.jpg~c200',
+  'https://s-media-cache-ak0.pinimg.com/736x/c4/df/97/c4df974ed6a9144b38e31360912898eb.jpg',
+  'http://ekladata.com/aNG7q920ZA59GLvdh0SjQhXrWMs@200x200.jpg',
+  'http://ekladata.com/-jQr-XuNktvvO6f4lo3CMMVK7ew@200x200.png',
+  'http://neverpedia.com/wiki/images/thumb/2/21/DisneyPeterPan.jpg/200px-DisneyPeterPan.jpg',
+  'https://www.ecranlarge.com/media/cache/200x200/uploads/image/000/938/pinocchio-photo-pinocchio-938230.jpg'
 ];
 var pixar = [
   'https://33hpwq10j9luq8gl43e62q4e-wpengine.netdna-ssl.com/wp-content/uploads/2017/04/0GCwOihe9dZPvHZrY.jpg',
@@ -113,103 +113,114 @@ var pixar = [
   'http://trus.imageg.net/graphics/corp/2934995_Image_1.jpg',
   'https://cdn3.whatculture.com/images/2011/06/Mr-Incredible.jpg'
 ];
-var harryPotter = [
-  
-];
+var harryPotter = [];
 
 var images = [],
     theme = "",
     count = 19,
     rand = 0,
-    temp1 = "",
-    temp2 = "",
-    click = 0,
+    tempElt1 = "",
+    tempElt2 = "",
+    click = -1,
     score = 0,
     win = 0,
     time = 0;
 
-var $themes = $(".themes"),
-    $pre = $("#pre"),
-    $box = $(".box"),
-    $time = $("#time"),
-    $score = $("#score"),
-    $post = $("#post"),
-    $final = $("#final");
+var preElt = document.querySelector("#pre"),
+    themesElts = document.querySelectorAll(".themes"),
+    boxElts = document.querySelectorAll(".box"),
+    timeElt = document.querySelector("#time"),
+    scoreElt = document.querySelector("#score"),
+    postElt = document.querySelector("#post"),
+    finalElt = document.querySelector("#final");
 
 // initiate the game with chosen theme
-$themes.on("click", function () {
-  theme = $(this).attr("id");
-  // insert theme in images array
-  for (j=0; j<20; j++) {
-    if (theme === "pokemon") {
-      images.push(pokemon[j]);
+for (let j=0; j<themesElts.length; j++) {
+  themesElts[j].addEventListener("click", function () {
+    theme = this.id;
+    // insert theme in images array
+    switch (theme) {
+      case "pokemon":
+        for (let i=0; i<20; i++) {images.push(pokemon[i]);}
+        break;
+      case "starwars":
+        for (let i=0; i<20; i++) {images.push(starwars[i]);}
+        break;
+      case "lotr":
+        for (let i=0; i<20; i++) {images.push(lotr[i]);}
+        break;
+      case "disney":
+        for (let i=0; i<20; i++) {images.push(disney[i]);}
+        break;
+      case "pixar":
+        for (let i=0; i<20; i++) {images.push(pixar[i]);}
+        break;
     }
-    else if (theme === "starWars") {
-      images.push(starWars[j]);
+
+    // insert images in memory game
+    for (let i=0; i<20; i++) {
+      rand = Math.floor(Math.random() * count);
+      boxElts[i].innerHTML = "<img src='" + images[rand] + "' alt='image' class='hidden'>";
+      images.splice(rand, 1);
+      count--;
     }
-    else if (theme === "lotr") {
-      images.push(lotr[j]);
-    }
-    else if (theme === "disney") {
-      images.push(disney[j]);
-    }
-    else if (theme === "pixar") {
-      images.push(pixar[j]);
-    }
-  }
-  // insert images in memory game
-  for (i=0; i<20; i++) {
-    rand = Math.floor(Math.random() * count);
-    $box.eq(i).html("<img src='" + images[rand] + "' alt='image' class='hidden'>");
-    images.splice(rand, 1);
-    count--;
-  }
-  $pre.fadeOut();
-});
+    preElt.style.display = "none";
+  });
+}
+
 
 // Handle the play
-$(".play").on("click", function () {
-  $(this).find("img").fadeIn();
-  $(this).find("img").addClass("current");
-  // first of two click
-  if (click % 2 === 0 || click === 0) {
-    temp1 = $(this).find("img").attr("src");
-    // timer
-    if (click === 0) {
-      timer = setInterval(function() {
-        time++;
-        $time.html(time + " sec");
-      }, 1000);
-    }
-  }
-  // second click
-  else {
-    temp2 = $(this).find("img").attr("src");
-    // different images
-    if (temp1 !== temp2) {
-      $(".current").fadeOut();
-      $(".current").removeClass("current");
-      if (score > 0){
-        score -= 2;
+for (let j=0; j<document.querySelectorAll(".play").length; j++) {
+  document.querySelectorAll(".play")[j].addEventListener("click", function () {
+    this.firstChild.classList.remove("hidden");
+
+    // first of two click
+    if (click < 1) {
+      tempElt1 = this;
+      // timer
+      if (click === -1) {
+        timer = setInterval(function() {
+          time++;
+          timeElt.innerHTML = time + " sec";
+        }, 1000);
       }
-      $score.html("score: " + score);
+      click = 1;
     }
-    // same images
-    else {
-      score += 10;
-      win += 2;
-      $(".current").css("outline", "solid 2px #34495e");
-      $(".current").parent().removeClass("play");
-      $(".current").parent().off("click");
-      $(".current").removeClass("current");
-      $score.html("score: " + score);
-      // game won
-      if (win === 20) {
-        clearTimeout(timer);
-        $final.html("You won " + score + " points <br> in " + time + " seconds");
-        $post.css("display", "flex");
+
+    // second click
+    else if (this !== tempElt1) {
+      tempElt2 = this;
+
+      // different images
+      if (tempElt1.firstChild.src !== tempElt2.firstChild.src) {
+        setTimeout( function() {
+          tempElt1.firstChild.classList.add("hidden");
+          tempElt2.firstChild.classList.add("hidden");
+        }, 400);
+        if (score > 0){
+          score -= 2;
+        }
+        scoreElt.innerHTML = "score: " + score;
       }
+
+      // same images
+      else {
+        score += 10;
+        win += 2;
+        tempElt1.firstChild.classList.add("outlined");
+        tempElt2.firstChild.classList.add("outlined");
+        tempElt1.classList.remove("play");
+        tempElt2.classList.remove("play");
+        scoreElt.innerHTML = "score: " + score;
+
+        // game won
+        if (win === 20) {
+          clearTimeout(timer);
+          finalElt.innerHTML = "You won " + score + " points <br> in " + time + " seconds";
+          postElt.style.display = "flex";
+        }
+      }
+      click = 0;
     }
-  }
-  click++;
-});
+  });
+}
