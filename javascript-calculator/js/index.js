cf472ec41a96ca6203573f0c0025b2450b5bd28f
Rémy Beumier <beumieremy@gmail.com>
2018-01-18 22:23:07 +0100

    calculator design improved

--- javascript-calculator/js/index.js
+++ javascript-calculator/js/index.js
@@ -1,81 +1,22 @@
 
-// // button shadow
-// $(".button").on("click", function () {
-//   $(this).css("box-shadow", "none");
-//   $(this).delay(100)
-//   .queue(function (next) { 
-//     $(this).css("box-shadow", "3px 3px 3px black"); 
-//     next();
-//   });
-// });
+var operationElts = document.querySelectorAll(".number, .operator"),
+    clearElt = document.querySelector("#clear"),
+    equalElt = document.querySelector("#equal"),
+    screenElt = document.querySelector("#screen");
 
 // CALCULATOR
 // display numbers and operators clicked
-$(".number, .operator").on("click", function () {
-  $("#screen").append($(this).html());
-})
+for (let i=0; i<operationElts.length; i++) {
+  operationElts[i].addEventListener("click", function() {
+    screenElt.insertAdjacentHTML("beforeEnd", this.innerHTML);
+  });
+}
 // clear the calc
-$("#clear").on("click", function () {
-  $("#screen").html("");
+clearElt.addEventListener("click", function() {
+  screenElt.innerHTML = "";
 });
 // process calc
-$("#equal").on("click", function () {
+equalElt.addEventListener("click", function() {
   // round answer to have max 6 decimals
-  $("#screen").html(Math.round(eval($("#screen").html()) * 1000000) / 1000000);
+  screenElt.innerHTML = Math.round(eval(screenElt.innerHTML) * 1000000) / 1000000;
 });
-
-
-// handle wrong input 
-// 2 operators
-// first is NaN
-
-
-/* old and way longer version
-// two empty var; 
-var curr = "";
-var glob = [];
-
-// number buttons
-$(".number").on("click", function () {
-  // deletes first 0
-  if (curr.length === 0 && glob.length === 0) {
-    $("#screen").html("");
-  }
-  $("#screen").append($(this).html());
-  curr += $(this).html();
-});
-
-// operator buttons
-$(".operator").on("click", function () {
-  glob.push(curr);
-  $("#screen").append($(this).html());
-  glob.push($(this).html());
-  curr = "";
-});
-
-// clear button
-$("#clear").on("click", function () {
-  $("#screen").html("0");
-  // reset var
-  curr = "";
-  glob = [];
-});
-
-// equal button
-$("#equal").on("click", function() {
-  glob.push(curr);
-  $("#screen").html(glob);
-  //var calc = [];
-  var calc = "";
-  // go through array made of numbers and operators
-  for (i=0; i<glob.length; i++) {
-    calc += glob[i];
-  }
-  // turn string into calc and round it with max 9 decimals
-  var result = Math.round(eval(calc) * 1000000000) / 1000000000;
-  $("#screen").html(result);
-  // reset arrays
-  curr = "";
-  glob = [];
-});
-*/
