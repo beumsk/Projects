807739cb4ea30885f448b50ce95605160f7b88c4
Rémy Beumier <beumieremy@gmail.com>
2018-02-14 19:46:18 +0100

    to do list jquery replaced with vanilla js and design improved

--- to-do-list/README.txt
+++ to-do-list/README.txt
@@ -1,3 +1,3 @@
 A Pen created at CodePen.io. You can find this one at https://codepen.io/beumsk/pen/QvqyMM.
 
- A reactive To Do List created with html-css-js.
+ A reactive To Do List created with html, css and javascript.
--- to-do-list/js/index.js
+++ to-do-list/js/index.js
@@ -1,26 +1,79 @@
-$("button").on("click", function(e) {
+// TODO: use local storage => https://www.taniarascia.com/how-to-use-local-storage-with-javascript/
+
+// var buttonElt = document.querySelector("#button"),
+//     inputElt = document.querySelector("#inp"),
+//     bottomElt = document.querySelector("#bottom"),
+//     count = -1,
+//     // checks if there is local storage
+//     itemsArr = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+
+// localStorage.setItem('items', JSON.stringify(itemsArr));
+// var data = JSON.parse(localStorage.getItem('items'));
+
+// function addElt(text) {
+//   bottomElt.insertAdjacentHTML("beforeEnd", "<p class='elt'>" + text + "<span class='del' title='Delete element'>x</span></p>");
+// };
+
+// // input handler; add to do elements
+// buttonElt.addEventListener("click", function(e) {
+//   e.preventDefault();
+//   var inputVal = inputElt.value;
+  
+//   // local storage lines
+//   itemsArr.push(inputVal);
+//   localStorage.setItem('items', JSON.stringify(itemsArr));
+  
+//   addElt(inputVal);
+//   inputElt.value = "";
+//   count++;  
+  
+//   // mark element as done
+//   document.querySelectorAll(".elt")[count].addEventListener("click", function() {
+//     this.classList.toggle("crossed");
+//   });
+  
+//   // hide and delete element
+//   document.querySelectorAll(".del")[count].addEventListener("click", function() {
+//     var currentElt = this.parentNode;
+//     currentElt.style.display = "none";
+//     currentElt.parentNode.removeChild(currentElt);
+//     count--;
+//   });
+// });
+  
+// // add element
+// data.forEach(item => {
+//   addElt(item);
+// });
+  
+
+
+
+var buttonElt = document.querySelector("#button"),
+    inputElt = document.querySelector("#inp"),
+    bottomElt = document.querySelector("#bottom"),
+    count = -1;
+
+// input handler; add to do elements
+buttonElt.addEventListener("click", function(e) {
   e.preventDefault();
-});
+  var inputVal = inputElt.value;
+  if (inputVal.length > 0) {
+    bottomElt.insertAdjacentHTML("beforeEnd", "<p class='elt'>" + inputVal + "<span class='del' title='Delete element'>x</span></p>");
+    inputElt.value = "";
+    count++;
 
-
-$(function() {
-  $("button").on("click", function() {
-    var input = $("#inp").val();
-    if (input.length > 0) {
-      $("#bottom").prepend("<p class='elt'>" + input + "<span class='del' title='Delete element'>x</span></p>");
-      $(".elt").eq(0).fadeIn(800);
-      $("#inp").val("");
-    }
-    
-    /*$(".elt").on("click", function() {
-      $(this).toggleClass("crossed");
-    });*/
-    
-    $(".del").on("click", function() {
-      $(this).parent().fadeOut(400);
-      $(this).remove();
+    // mark element as done
+    document.querySelectorAll(".elt")[count].addEventListener("click", function() {
+      this.classList.toggle("crossed");
     });
-  });
-  
-  
+
+    // hide and delete element
+    document.querySelectorAll(".del")[count].addEventListener("click", function() {
+      var currentElt = this.parentNode;
+      currentElt.style.display = "none";
+      currentElt.parentNode.removeChild(currentElt);
+      count--;
+    });
+  }
 });
