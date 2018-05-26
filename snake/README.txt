c706c03493a27a82496ffdad8e35e71896ea1f41
Rémy Beumier <beumieremy@gmail.com>
2018-05-26 17:54:26 +0200

    make snake work on mobile

--- snake/README.txt
+++ snake/README.txt
@@ -1,3 +1,4 @@
 A Pen created at CodePen.io. You can find this one at https://codepen.io/beumsk/pen/PmPxEo.
 
  The mythic Snake game created with html, css and vanilla javascript.
+Mobile swipe added through jQuery.
--- snake/js/index.js
+++ snake/js/index.js
@@ -1,12 +1,11 @@
 
-// add mobile moves
 // weird bug stopping the game
 // ADD time?
 
-var snakeTable = document.getElementsByClassName("snakeTable")[0],
+var snakeTable = document.querySelector(".snakeTable"),
     boxes = document.getElementsByClassName("box"),
-    modul = document.getElementsByClassName("modul")[0],
-    start = document.getElementsByClassName("start")[0];
+    modul = document.querySelector(".modul"),
+    start = document.querySelector(".start");
 
 var table = {
   rowsCols: 21,
@@ -96,7 +95,7 @@ function hitBorder() {
   let headPos = snake.position.length-1;
   // goes of limits
   if (((snake.position[headPos][0] === table.rowsCols-1) && (snake.direction === "right")) || ((snake.position[headPos][0] === 0) && (snake.direction === "left")) || ((snake.position[headPos][1] === table.rowsCols-1) && (snake.direction === "down")) ||  ((snake.position[headPos][1] === 0) && (snake.direction === "up"))) {
-    console.log("border hit");
+    // console.log("border hit");
     stopp();
   }
 }
@@ -106,7 +105,7 @@ function hitSnake() {
   let headPos = snake.position.length-1;
   for (let i=0; i<headPos; i++) {
     if (snake.position[headPos].toString() === snake.position[i].toString()) {
-      console.log("snake hit");
+      // console.log("snake hit");
       stopp();
     }
   } 
@@ -209,5 +208,120 @@ document.addEventListener("keydown", function(e) {
     snake.canTurn = 0;
   }
 });
+
+// swipe Showcase
+$("document").ready(function() {
+  $(".snakeTable")
+    .swipeDetector()
+    .on("swipeLeft.sd swipeRight.sd swipeUp.sd swipeDown.sd", function(event) {
+      if (event.type === "swipeLeft") {
+        if (snake.direction === "right") return
+        snake.direction = "left";
+      } else if (event.type === "swipeRight") {
+        if (snake.direction === "left") return
+        snake.direction = "right";
+      } else if (event.type === "swipeUp") {
+        if (snake.direction === "down") return
+        snake.direction = "up";
+      } else if (event.type === "swipeDown") {
+        if (snake.direction === "up") return
+        snake.direction = "down";
+      }
+      snake.canTurn = 0;
+    });
+});
+
+// swipe function --> credit: https://codepen.io/AlexEmashev/pen/BKgQdx?editors=0100
+(function($) {
+  $.fn.swipeDetector = function(options) {
+    // States: 0 - no swipe, 1 - swipe started, 2 - swipe released
+    var swipeState = 0;
+    // Coordinates when swipe started
+    var startX = 0;
+    var startY = 0;
+    // Distance of swipe
+    var pixelOffsetX = 0;
+    var pixelOffsetY = 0;
+    // Target element which should detect swipes.
+    var swipeTarget = this;
+    var defaultSettings = {
+      // Amount of pixels, when swipe don't count.
+      swipeThreshold: 30,
+      // Flag that indicates that plugin should react only on touch events.
+      // Not on mouse events too.
+      useOnlyTouch: true
+    };
+
+    // Initializer
+    (function init() {
+      options = $.extend(defaultSettings, options);
+      // Support touch and mouse as well.
+      swipeTarget.on("mousedown touchstart", swipeStart);
+      $("html").on("mouseup touchend", swipeEnd);
+      $("html").on("mousemove touchmove", swiping);
+    })();
+
+    function swipeStart(event) {
+      if (options.useOnlyTouch && !event.originalEvent.touches) return;
+
+      if (event.originalEvent.touches) event = event.originalEvent.touches[0];
+
+      if (swipeState === 0) {
+        swipeState = 1;
+        startX = event.clientX;
+        startY = event.clientY;
+      }
+    }
+
+    function swipeEnd(event) {
+      if (swipeState === 2) {
+        swipeState = 0;
+
+        if (
+          Math.abs(pixelOffsetX) > Math.abs(pixelOffsetY) &&
+          Math.abs(pixelOffsetX) > options.swipeThreshold
+        ) {
+          // Horizontal Swipe
+          if (pixelOffsetX < 0) {
+            swipeTarget.trigger($.Event("swipeLeft.sd"));
+          } else {
+            swipeTarget.trigger($.Event("swipeRight.sd"));
+          }
+        } else if (Math.abs(pixelOffsetY) > options.swipeThreshold) {
+          // Vertical swipe
+          if (pixelOffsetY < 0) {
+            swipeTarget.trigger($.Event("swipeUp.sd"));
+          } else {
+            swipeTarget.trigger($.Event("swipeDown.sd"));
+          }
+        }
+      }
+    }
+
+    function swiping(event) {
+      // If swipe don't occuring, do nothing.
+      if (swipeState !== 1) return;
+
+      if (event.originalEvent.touches) {
+        event = event.originalEvent.touches[0];
+      }
+
+      var swipeOffsetX = event.clientX - startX;
+      var swipeOffsetY = event.clientY - startY;
+
+      if (
+        Math.abs(swipeOffsetX) > options.swipeThreshold ||
+        Math.abs(swipeOffsetY) > options.swipeThreshold
+      ) {
+        swipeState = 2;
+        pixelOffsetX = swipeOffsetX;
+        pixelOffsetY = swipeOffsetY;
+      }
+    }
+
+    return swipeTarget; // Return element available for chaining.
+  };
+})(jQuery);
+
 
 // https://www.theodinproject.com/courses/javascript-and-jquery/lessons/jquery-and-the-dom
