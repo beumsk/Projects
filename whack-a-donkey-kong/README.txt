c6d18cdab8dd5b0cacf9c167c6bb52375cc4c73c
Rémy Beumier <beumieremy@gmail.com>
2019-01-22 09:16:14 +0100

    donkey kong updated

--- whack-a-donkey-kong/README.txt
+++ whack-a-donkey-kong/README.txt
@@ -1,4 +1,3 @@
 A Pen created at CodePen.io. You can find this one at https://codepen.io/beumsk/pen/YabyJJ.
 
- Whack a mole themed Donkey Kong created for the #CodePenChallenge 
-#cpc-dk
+ Whack a mole themed Donkey Kong created for the #CodePenChallenge #cpc-dk
--- whack-a-donkey-kong/js/index.js
+++ whack-a-donkey-kong/js/index.js
@@ -1,6 +1,4 @@
 
-// pre modal for instructions
-// end of game + reset
 // adapt speed to device
 // prevent multiple click by apparition
 
@@ -14,50 +12,74 @@ var timeElt = document.querySelector(".time");
 var modal = document.querySelector(".modal");
 var modalP = document.querySelector(".modal p");
 var modalBtn = document.querySelector(".modal .end");
+var setTimer, setShowHide;
 
+// play button
+modalBtn.addEventListener("click", function() {
+  clearInterval(checkPageInterval);
+  modal.classList.add("hidden");
+  setTimer = setInterval(timer, 1000);
+  setShowHide = setInterval(showHide, speed*2);
+});
 
 // click handler and score updater
 for(let i=0; i<images.length; i++) {
-  images[i].addEventListener("click", function() {
-    curr = this;
-    // show user he clicked well
-    curr.style.opacity = "0.7";
-    score += 10;
-    scoreElt.innerHTML = score;
-    setTimeout(function() {
-      curr.style.opacity = "1";
-    }, speed);
-  });
+  images[i].addEventListener("click", hitKong);
+}
+
+function hitKong(e) {
+  curr = e.target;
+  curr.parentNode.classList.add("touched");
+  score += 10;
+  scoreElt.innerHTML = score;
+  setTimeout(function() {
+    curr.parentNode.classList.remove("touched");
+  }, speed/2);
 }
 
 // show and hide with interval
-setInterval(function() {
+function showHide() {
   var randd = randomize(9);
   images[randd].style.top = "30px";
   setTimeout(function() {
     images[randd].style.top = "100px";
   }, speed);
-}, speed*2);
+}
 
 // manage timer
-setInterval(function() {
+function timer() {
   if (time) {
     time -= 1;
     timeElt.innerHTML = time;
   }
   else {
-    // show score and reload
-    modalP.innerHTML = "You scored " + score + " !";
-    modal.style.display = "flex";
+    restart();
   }
-}, 1000);
+}
 
-// play again button
-modalBtn.addEventListener("click", function() {
-  document.location.reload(true);
-});
+function restart() {
+  modalP.innerHTML = "You scored " + score + " !";
+  modal.classList.remove("hidden");
+  clearInterval(setTimer);
+  clearInterval(setShowHide);
+  time = 30;
+  score = 0;
+  timeElt.innerHTML = time;
+  scoreElt.innerHTML = score;
+}
 
 // function random
 function randomize(rand) {
   return Math.floor(Math.random() * rand);
 }
+
+// handle focus of the page
+function checkPageFocus() {
+  if (document.hasFocus()) {
+    modal.classList.remove("hidden");
+  }
+  else {
+    modal.classList.add("hidden");
+  }
+}
+var checkPageInterval = setInterval(checkPageFocus, 300);
