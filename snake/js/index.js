1e0e765b12ff1651393e9d02c0e09620771cd83f
Rémy Beumier <beumieremy@gmail.com>
2019-02-03 14:33:56 +0100

    snake fixed

--- snake/js/index.js
+++ snake/js/index.js
@@ -1,11 +1,12 @@
 
-// weird bug stopping the game
+// prevent food from popping on snake <> new food is unseen until snake moves from food box TESTIT!!
 // ADD time?
+// increase speed over time/food
 
-var snakeTable = document.querySelector(".snakeTable"),
-    boxes = document.getElementsByClassName("box"),
-    modul = document.querySelector(".modul"),
-    start = document.querySelector(".start");
+var snakeTable = document.querySelector(".snakeTable");
+var boxes = document.getElementsByClassName("box");
+var modul = document.querySelector(".modul");
+var start = document.querySelector(".start");
 
 var table = {
   rowsCols: 21,
@@ -35,16 +36,27 @@ var snake = {
 // init game
 snake.init();
 
+
+start.addEventListener("click", startSnake);
+
+document.addEventListener("keydown", function(e) {
+  if (e.keyCode === 13 && snake.time === 0) {
+    startSnake();
+  }
+});
+
 // start game
-start.addEventListener("click", function() {
-  modul.style.display = "none";
+function startSnake() {
+  modul.classList.add("hidden");
+  clearInterval(checkPageInterval);
+  snake.time = 1;
   renderSnake();
   randomFood();
   // interval, heart of the game
   setInt = setInterval(function() {
     move();
   }, snake.speed);
-});
+}
 
 // end of game
 function stopp() {
@@ -55,21 +67,28 @@ function stopp() {
     start.querySelector("span").innerHTML = "Play Snake";
   }, 800);
   snake.init();
-  modul.style.display = "flex";
+  modul.classList.remove("hidden");
 }
 
 // move the snake function
 function move() {
-  snake.canTurn = 1;
-  // check if move allowed
+  // check if move allowed & then eat food
   hitFood();
   hitBorder();
   hitSnake();
+  // actually move the snake
+  updatePositions();
+  renderSnake();
+  document.addEventListener("keydown", turn);
+  snake.canTurn = 1;
+}
+
+function updatePositions() {
   // remove last snake part (first snake pos)
   boxes[snake.position[0][0] + snake.position[0][1] * table.rowsCols].classList.remove("snake");
   snake.position.shift();
-  let head = snake.position[snake.position.length - 1];
   // add new snake part
+  var head = snake.position[snake.position.length - 1];
   switch (snake.direction) {
     case "left":
       snake.position.push([head[0] - 1, head[1]]);
@@ -86,26 +105,24 @@ function move() {
     default:
       console.log("no direction !");
   }
-  // read positions and render snake parts
-  renderSnake();
 }
 
 // checks border contact
 function hitBorder() {
-  let headPos = snake.position.length-1;
+  var headPos = snake.position.length-1;
   // goes of limits
   if (((snake.position[headPos][0] === table.rowsCols-1) && (snake.direction === "right")) || ((snake.position[headPos][0] === 0) && (snake.direction === "left")) || ((snake.position[headPos][1] === table.rowsCols-1) && (snake.direction === "down")) ||  ((snake.position[headPos][1] === 0) && (snake.direction === "up"))) {
-    // console.log("border hit");
+    console.log("border hit");
     stopp();
   }
 }
 
 // checks self contact
 function hitSnake() {
-  let headPos = snake.position.length-1;
-  for (let i=0; i<headPos; i++) {
+  var headPos = snake.position.length-1;
+  for (var i=0; i<headPos; i++) {
     if (snake.position[headPos].toString() === snake.position[i].toString()) {
-      // console.log("snake hit");
+      console.log("snake hit");
       stopp();
     }
   } 
@@ -113,26 +130,11 @@ function hitSnake() {
 
 // checks food contact
 function hitFood() {
-  let head = snake.position[snake.position.length-1];
-  let tail = snake.position[0];
+  var head = snake.position[snake.position.length-1];
+  var tail = snake.position[0];
   if (head.toString() === foodPos.toString()) {
     boxes[random].classList.remove("food");
-    switch (snake.direction) {
-      case "left":
-        snake.position.unshift([tail[0] + 1, tail[1]]);
-        break;
-      case "up":
-        snake.position.unshift([tail[0], tail[1] + 1]);
-        break;
-      case "right":
-        snake.position.unshift([tail[0] - 1, tail[1]]);
-        break;
-      case "down":
-        snake.position.unshift([tail[0], tail[1] - 1]);
-        break;
-      default:
-        console.log("no direction !");
-    }
+    snake.position.unshift(tail);
     randomFood();
     snake.score += 10;
     scoreElt.innerHTML = snake.score + " pts";
@@ -143,15 +145,49 @@ function hitFood() {
 function randomFood() {
   var randomX = Math.floor(Math.random() * table.rowsCols);
   var randomY = Math.floor(Math.random() * table.rowsCols);
+  while (snake.position.indexOf([randomX, randomY]) !== -1) {
+    randomX = Math.floor(Math.random() * table.rowsCols);
+    randomY = Math.floor(Math.random() * table.rowsCols);
+  }
   random = randomX + randomY * table.rowsCols;
   boxes[random].classList.add("food");
   foodPos = [randomX, randomY];
 }
 
-// render the snake
+// read positions and render the snake
 function renderSnake() {
-  for (let i=0; i<snake.position.length; i++) {
+  for (var i=0; i<snake.position.length; i++) {
     boxes[snake.position[i][0] + snake.position[i][1] * table.rowsCols].classList.add("snake");
+  }
+}
+
+// keypress handling to turn
+function turn(e) {
+  if (snake.canTurn) {
+    switch (e.keyCode) {
+      case 13:
+        // document.removeEventListener()
+        break;
+      case 37:// left
+        if (snake.direction === "right") return;
+        snake.direction = "left";
+        break;
+      case 38:// up
+        if (snake.direction === "down") return;
+        snake.direction = "up";
+        break;
+      case 39:// right
+        if (snake.direction === "left") return;
+        snake.direction = "right";
+        break;
+      case 40:// down
+        if (snake.direction === "up") return;
+        snake.direction = "down";
+        break;
+      default:
+        console.log("wrong key");
+    }
+    snake.canTurn = 0;
   }
 }
 
@@ -159,7 +195,7 @@ function renderSnake() {
 function tableCreation() {
   if (snakeTable.innerHTML === "") {
     // main table
-    for (let i = 0; i<table.boxes; i++) {
+    for (var i = 0; i<table.boxes; i++) {
       var divElt = document.createElement("div");
       divElt.classList.add("box");
       snakeTable.appendChild(divElt);
@@ -175,56 +211,33 @@ function tableCreation() {
   }
 }
 
-// keypress function
-document.addEventListener("keydown", function(e) {
-  if (snake.canTurn) {
-    switch (e.keyCode) {
-      case 13:
-        stopp();
-        break;
-      // left
-      case 37:
-        if (snake.direction === "right") return;
-        snake.direction = "left";
-        break;
-      // up
-      case 38:
-        if (snake.direction === "down") return;
-        snake.direction = "up";
-        break;
-      // right
-      case 39:
-        if (snake.direction === "left") return;
-        snake.direction = "right";
-        break;
-      // down
-      case 40:
-        if (snake.direction === "up") return;
-        snake.direction = "down";
-        break;
-      default:
-        console.log("wrong key");
-    }
-    snake.canTurn = 0;
+// handle focus of the page
+function checkPageFocus() {
+  if (document.hasFocus()) {
+    modul.classList.remove("hidden");
   }
-});
+  else {
+    modul.classList.add("hidden");
+  }
+}
+var checkPageInterval = setInterval(checkPageFocus, 300);
 
 // swipe Showcase
 $("document").ready(function() {
-  $(".snakeTable")
+  $("body")
     .swipeDetector()
     .on("swipeLeft.sd swipeRight.sd swipeUp.sd swipeDown.sd", function(event) {
       if (event.type === "swipeLeft") {
-        if (snake.direction === "right") return
+        if (snake.direction === "right") return;
         snake.direction = "left";
       } else if (event.type === "swipeRight") {
-        if (snake.direction === "left") return
+        if (snake.direction === "left") return;
         snake.direction = "right";
       } else if (event.type === "swipeUp") {
-        if (snake.direction === "down") return
+        if (snake.direction === "down") return;
         snake.direction = "up";
       } else if (event.type === "swipeDown") {
-        if (snake.direction === "up") return
+        if (snake.direction === "up") return;
         snake.direction = "down";
       }
       snake.canTurn = 0;
@@ -323,5 +336,14 @@ $("document").ready(function() {
   };
 })(jQuery);
 
+// remove scroll for mobile IOS issue
+function preventDefault(e){e.preventDefault();}
+function disableScroll(){
+    document.body.addEventListener('touchmove', preventDefault, { passive: false });
+}
+function enableScroll(){
+    document.body.removeEventListener('touchmove', preventDefault, { passive: false });
+}
+disableScroll();
 
 // https://www.theodinproject.com/courses/javascript-and-jquery/lessons/jquery-and-the-dom
