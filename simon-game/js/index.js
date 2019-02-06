e8becb60a1b00d2dfa1a0fc2d7d774565f0d9bb3
Beumier, Rémy <beumieremy@gmail.com>
2019-02-06 09:56:59 +0100

    simon game to vanilla JS

--- simon-game/js/index.js
+++ simon-game/js/index.js
@@ -1,163 +1,224 @@
 
-$(document).ready(function () {
-  
-  // game variables
-  var comp = [],
-      player = [],
-      count = 0,
-      j = 0,
-      strict = false,
-      divs = ["green", "red", "yellow", "blue"],
-      greenAudio = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3"),
-      redAudio = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3"),
-      yellowAudio = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3"),
-      blueAudio = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3");
-  
-  // jQuery variables
-  var $start = $("#start"),
-      $strict = $("#strict"),
-      $startStrict = $("#start, #strict"),
-      $on = $("#on"),
-      $off = $("#off"),
-      $onOff = $("#on, #off"),
-      $colors = $(".colors"),
-      $count = $("#count"),
-      $green = $("#green"),
-      $red = $("#red"),
-      $yellow = $("#yellow"),
-      $blue = $("#blue");
-  
-  // switch on/off
-  $onOff.on("click", function () {
-    $on.toggleClass("on");
-    $off.toggleClass("off");
-    if ($on.hasClass("on")) {
-      $count.val("--");
-      $startStrict.removeAttr("disabled");
-    }
-    if ($off.hasClass("off")) {
-      $count.val("");
-      $startStrict.attr("disabled", "disabled");
-      $startStrict.removeClass("start-strict");
-    }
-  });
-  
-  // reset colors
-  function resetColor() {
-    $green.css("background", "#27ae60");
-    $red.css("background", "#c0392b");
-    $yellow.css("background", "#f39c12");
-    $blue.css("background", "#2980b9");
-  }
-  
-  // random color picker
-  function randomizer() { 
-    count++;    
-    $count.val(count);
-    var length = $colors.length;
-    var random = Math.floor(Math.random() * length);
-    comp.push(divs[random]);
-    displaySet(comp);
-  }
-  
-  // current color animation
-  function currentDisplay(color) {
-    switch (color) {
-      case "green":
-        $green.css("background", "#2ecc71");
-        greenAudio.play();
-        break;
-      case "red":
-        $red.css("background", "#e74c3c");
-        redAudio.play();
-        break;
-      case "yellow":
-        $yellow.css("background", "#f1c40f");
-        yellowAudio.play();
-        break;
-      case "blue":
-        $blue.css("background", "#3498db");
-        blueAudio.play();
-        break;
-    }
-    window.setTimeout(function() {
-      resetColor();
-    }, 300);
-  }
-  
-  // run the color list
-  function displaySet(array) {
-    var i = 0;
-    var interval = setInterval(function() {
-      currentDisplay(array[i]);
-      i++;
-      if (i >= array.length) {
-        clearInterval(interval);
-      }
-    }, 650);
-  }
-  
-  // checks player action
-  function checkIds() {
-    $colors.click(function() {
-      var ID = $(this).attr("id");
-      currentDisplay(ID);
-      player.push(ID);
-      
-      if (ID !== comp[j]) {
-        j = 0;
-        $count.val("X");
-        $colors.off();
-        if(strict){
-          return false;
-        }
-        setTimeout(function() {
-          $count.val(count);
-          displaySet(comp);
-          checkIds();
-        }, 1000);
+// fix mobile audio delay
 
-      } else if (typeof comp[20] !== 'undefined'){
-        $colors.off();
-        $count.val("GG");
-        
-      } else if (typeof comp[j+1] === 'undefined'){
-        $colors.off();
-        setTimeout(function() {
-          j=0;
-          randomizer();
-          checkIds();
-        }, 1000);
-      }
-      else{
-        j++;
-      }
-    });
-  }  
-  
-  // set/reset everything 
-  function reset(){
-    resetColor();
-    $colors.off();
-    comp = []; count = 0; j = 0; player = [];
-    randomizer();
-    displaySet(comp);
-    checkIds();
-    $startStrict.removeClass("start-strict");
+var index = 0,
+    strict = 0,
+    compColors = [],
+    // playerColors = [],
+    colors = ["green", "red", "yellow", "blue"];
+
+var onOffElt = document.querySelector("#on-off"),
+    levelElt = document.querySelector("#level"),
+    basicElt = document.querySelector("#basic"),
+    strictElt = document.querySelector("#strict"),
+    colorElts = document.querySelectorAll(".colors");
+
+// on off button
+onOffElt.addEventListener("click", function(e) {
+  // turning ON
+  if (!this.childNodes[1].classList.contains("active")) {
+    onOffElt.classList.remove("glow");
+    reset();
+    this.childNodes[1].classList.add("active");
+    this.childNodes[3].classList.remove("active");
+    levelElt.value = "--";
+    basicElt.addEventListener("click", start);
+    strictElt.addEventListener("click", start);
   }
-  
-  // normal mode button
-  $start.on("click", function sta() {
-    strict = false;
+  //turning OFF
+  else if (!this.childNodes[3].classList.contains("active")) {
     reset();
-    $(this).addClass("start-strict");
-  });
-  
-  // strict mode button
-  $strict.on("click", function str(){
-    strict = true;
-    reset();
-    $(this).addClass("start-strict");
-  });
-  
+    turnOff();
+  }
 });
+ 
+// start when basic or strict pressed
+function start(e) {
+  var curr = e.target;
+  if (curr.getAttribute("id") == "basic") {
+    if (!curr.classList.contains("active")) {
+      reset();
+      curr.classList.add("active");
+      strictElt.classList.remove("active");
+      goo();
+    }
+    else {
+      curr.classList.remove("active");
+    }
+  }
+  else if (curr.getAttribute("id") == "strict") {
+    if (!curr.classList.contains("active")) {
+      reset();
+      strict = 1;
+      curr.classList.add("active");
+      basicElt.classList.remove("active");
+      goo();
+    }
+    else {
+      strict = 0;
+      curr.classList.remove("active");
+    }
+  }
+}
+
+function goo() {
+  randomizer();
+  playList(compColors);
+  addColorEvents();
+}
+
+// gets a random color and updates round number
+function randomizer() {
+  var random = Math.floor(Math.random() * 4);
+  compColors.push(colors[random]);
+  levelElt.value = compColors.length;
+  return compColors;
+}
+
+function playList(array) {
+  var i = 0;
+  var interval = setInterval(function() {
+    playSong(array[i]);
+    i++;
+    if (i >= array.length) {
+      clearInterval(interval);
+    }
+  }, 650);
+}
+
+function playSong(color) {
+  if (color === "green") {
+    greenAudio = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound1.mp3");
+    greenAudio.play();
+    colorElts[0].classList.add("active");
+    setTimeout(function() {
+      colorElts[0].classList.remove("active");
+    }, 200);
+  }
+  if (color === "red") {
+    redAudio = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound2.mp3");
+    redAudio.play();
+    colorElts[1].classList.add("active");
+    setTimeout(function() {
+      colorElts[1].classList.remove("active");
+    }, 200);
+  }
+  if (color === "yellow") {
+    yellowAudio = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound3.mp3");
+    yellowAudio.play();
+    colorElts[2].classList.add("active");
+    setTimeout(function() {
+      colorElts[2].classList.remove("active");
+    }, 200);
+  }
+  if (color === "blue") {
+    blueAudio = new Audio("https://s3.amazonaws.com/freecodecamp/simonSound4.mp3");
+    blueAudio.play();
+    colorElts[3].classList.add("active");
+    setTimeout(function() {
+      colorElts[3].classList.remove("active");
+    }, 200);
+  }
+}
+
+function checker(e) {
+  var currColor = e.target.getAttribute("id");
+  playSong(currColor);
+  // playerColors.push(currColor);
+
+  // failed the serie
+  if (currColor !== compColors[index]) {
+    index = 0;
+    levelElt.value = "X";
+    removeColorEvents();
+    if (strict) {
+      reset();
+      strict = 1;
+      setTimeout(function () {
+        levelElt.value = "--";
+        goo();
+      }, 1000);
+      return false;
+    }
+    setTimeout(function() {
+      levelElt.value = compColors.length;
+      playList(compColors);
+      addColorEvents();
+    }, 1000);
+  }
+  // win the serie
+  else if (typeof compColors[index+1] === "undefined") {
+    removeColorEvents();
+    // win the game
+    if (typeof compColors[19] !== "undefined") {
+      removeColorEvents();
+      levelElt.value = "GG";
+      setTimeout(function() {
+        winAnimation();
+      }, 500);
+      return false;
+    }
+    setTimeout(function() {
+      index = 0;
+      goo();
+    }, 1000);
+  }
+  // win a sound
+  else {
+    index++;
+  }
+}
+
+function addColorEvents() {
+  for (var j=0; j<colorElts.length; j++) {
+    colorElts[j].addEventListener("click", checker);
+  }
+}
+
+function removeColorEvents() {
+  for (var i=0; i<colorElts.length; i++) {
+    colorElts[i].removeEventListener("click", checker);
+  }
+}
+
+function winAnimation() {
+  var iterations = 3;
+  var sett = setInterval(function() {
+    colorElts[0].classList.add("active");
+    colorElts[1].classList.add("active");
+    colorElts[2].classList.add("active");
+    colorElts[3].classList.add("active");
+    setTimeout(function(){
+      colorElts[0].classList.remove("active");
+      colorElts[1].classList.remove("active");
+      colorElts[2].classList.remove("active");
+      colorElts[3].classList.remove("active");
+    }, 200);
+    iterations--;
+    if (iterations < 1) {
+      clearInterval(sett);
+    }
+  }, 400);
+  setTimeout(function() {
+    reset();
+    turnOff();
+  }, 1600);
+}
+
+function reset() {
+  compColors = [];
+  // playerColors = [];
+  j = 0;
+  strict = 0;
+  removeColorEvents()
+}
+
+function turnOff() {  
+  levelElt.value = "";
+  basicElt.removeEventListener("click", start);
+  strictElt.removeEventListener("click", start);
+  basicElt.classList.remove("active");
+  strictElt.classList.remove("active");
+  onOffElt.childNodes[1].classList.remove("active");
+  onOffElt.childNodes[3].classList.add("active");
+}
