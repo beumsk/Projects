ac09f4005a87d66abc88f930e8d6e420e4792f49
Rémy Beumier <beumieremy@gmail.com>
2020-06-18 14:37:53 +0200

    giphy search back (mess with todo list)

--- giphy-search/README.markdown
+++ giphy-search/README.markdown
@@ -1,4 +1,7 @@
-# To Do List
- _A Pen created at CodePen.io. Original URL: [https://codepen.io/beumsk/pen/QvqyMM](https://codepen.io/beumsk/pen/QvqyMM).
+# Giphy Search
 
- A reactive To Do List created with html, css and javascript.
+A Pen created on CodePen.io. Original URL: [https://codepen.io/beumsk/pen/EExyvQ](https://codepen.io/beumsk/pen/EExyvQ).
+
+Giphy search lets you find an adequate gif to your needs.
+Built with html, css and javascript (jquery)
+Used Giphy API
--- giphy-search/script.js
+++ giphy-search/script.js
@@ -1,79 +1,36 @@
-// TODO: use local storage => https://www.taniarascia.com/how-to-use-local-storage-with-javascript/
+// check other info returned by the API ???
 
-// var buttonElt = document.querySelector("#button"),
-//     inputElt = document.querySelector("#inp"),
-//     bottomElt = document.querySelector("#bottom"),
-//     count = -1,
-//     // checks if there is local storage
-//     itemsArr = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+var $form = $("form");
+var $search = $(".search");
+var $clear = $(".clear");
+var $giphy = $(".giphy img");
+var $giphyLink = $(".giphy a");
 
-// localStorage.setItem('items', JSON.stringify(itemsArr));
-// var data = JSON.parse(localStorage.getItem('items'));
-
-// function addElt(text) {
-//   bottomElt.insertAdjacentHTML("beforeEnd", "<p class='elt'>" + text + "<span class='del' title='Delete element'>x</span></p>");
-// };
-
-// // input handler; add to do elements
-// buttonElt.addEventListener("click", function(e) {
-//   e.preventDefault();
-//   var inputVal = inputElt.value;
-  
-//   // local storage lines
-//   itemsArr.push(inputVal);
-//   localStorage.setItem('items', JSON.stringify(itemsArr));
-  
-//   addElt(inputVal);
-//   inputElt.value = "";
-//   count++;  
-  
-//   // mark element as done
-//   document.querySelectorAll(".elt")[count].addEventListener("click", function() {
-//     this.classList.toggle("crossed");
-//   });
-  
-//   // hide and delete element
-//   document.querySelectorAll(".del")[count].addEventListener("click", function() {
-//     var currentElt = this.parentNode;
-//     currentElt.style.display = "none";
-//     currentElt.parentNode.removeChild(currentElt);
-//     count--;
-//   });
-// });
-  
-// // add element
-// data.forEach(item => {
-//   addElt(item);
-// });
-  
-
-
-
-var buttonElt = document.querySelector("#button"),
-    inputElt = document.querySelector("#inp"),
-    bottomElt = document.querySelector("#bottom"),
-    count = -1;
-
-// input handler; add to do elements
-buttonElt.addEventListener("click", function(e) {
+// launch function on form submit
+$form.on("submit", function(e) {
   e.preventDefault();
-  var inputVal = inputElt.value;
-  if (inputVal.length > 0) {
-    bottomElt.insertAdjacentHTML("beforeEnd", "<p class='elt'>" + inputVal + "<span class='del' title='Delete element'>x</span></p>");
-    inputElt.value = "";
-    count++;
-
-    // mark element as done
-    document.querySelectorAll(".elt")[count].addEventListener("click", function() {
-      this.classList.toggle("crossed");
-    });
-
-    // hide and delete element
-    document.querySelectorAll(".del")[count].addEventListener("click", function() {
-      var currentElt = this.parentNode;
-      currentElt.style.display = "none";
-      currentElt.parentNode.removeChild(currentElt);
-      count--;
-    });
-  }
+  goGiphy();
 });
+
+// clear input on click
+$clear.on("click", function() {
+  $search.val("");
+})
+
+function goGiphy() {
+  var input = $search.val();
+  // Ajax call to giphy API  
+  $.getJSON("https://api.giphy.com/v1/gifs/translate?api_key=bb2006d9d3454578be1a99cfad65913d&s=" + input, function(json) {
+    data = JSON.parse(JSON.stringify(json));
+    imgSrc = data.data.images.original.url;
+    $giphy.fadeOut(1000);
+    setTimeout( function() {
+      $giphy.attr("src", imgSrc);
+      $giphyLink.attr("href", imgSrc);
+      setTimeout( function() {
+        $giphy.addClass("gif");
+        $giphy.fadeIn(1000);
+      }, 800);
+    }, 800);
+  })
+}
